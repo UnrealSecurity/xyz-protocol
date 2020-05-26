35a08fa2a2afafe58ddcc8b966a4f43782033f4e
UnrealSec <arran.bishop89@aol.com>
2020-05-26 16:30:15 +0300

    Xyz

---
+++ XyzSharp/XyzKeyExchange.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+
+namespace XyzSharp
+{
+    class XyzKeyExchange
+    {
+        byte[] public_key = null;
+        byte[] remote_public_key = null;
+        ECDiffieHellmanCng ecdh;
+
+        public XyzKeyExchange()
+        {
+            this.ecdh = new ECDiffieHellmanCng();
+            this.ecdh.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            this.ecdh.HashAlgorithm = CngAlgorithm.Sha256;
+            this.public_key = this.ecdh.PublicKey.ToByteArray();
+        }
+
+        public byte[] GetLocalPublicKey()
+        {
+            return this.public_key;
+        }
+
+        public void SetRemotePublicKey(byte[] remote_public_key)
+        {
+            this.remote_public_key = remote_public_key;
+        }
+
+        public byte[] GetSharedSecretKey()
+        {
+            return this.ecdh.DeriveKeyMaterial(CngKey.Import(remote_public_key, CngKeyBlobFormat.EccPublicBlob));
+        }
+    }
+}
