74ac4538c76263a5d9e197d2c6b3dbf49da07103
UnrealSec <arran.bishop89@aol.com>
2020-10-13 05:49:58 +0300

    Fix

--- XyzSharp/XyzClient.cs
+++ XyzSharp/XyzClient.cs
@@ -41,6 +41,8 @@ namespace XyzSharp
         {
             this.client = client;
             this.stream = client.GetStream();
+
+            Read();
         }
 
         public async void Read()
